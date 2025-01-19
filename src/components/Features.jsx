dcf94b6d11203f19da8d16e8802ed943790db723
youssef hataba <joohataba@gmail.com>
2025-01-19 09:18:10 +0200

    add fotter

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -73,7 +73,7 @@ const Features = () => {
             src="/videos/feature-1.mp4"
             title={
               <p>
-                radi<b>n</b>t
+                radia<b>n</b>t
               </p>
             }
             description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,33 @@
+import {FaDiscord, FaGithub, FaTwitch, FaTwitter} from "react-icons/fa";
+
+const links = [
+  {href: "https://discord.com", icon: <FaDiscord />},
+  {href: "https://twitter.com", icon: <FaTwitter />},
+  {href: "https://twitch.com", icon: <FaTwitch />},
+  {href: "https://githup.com", icon: <FaGithub />},
+];
+
 const Footer = () => {
   return (
-  <footer></footer>
-  )
-}
+    <footer className="w-screen bg-violet-300 py-4 text-black">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm md:text-left">&copy; Zentry 2024. All rights reserved</p>
+        <div className="flex justify-center gap-4 md:justify-start">
+          {links.map((link) => (
+            <a
+              href={link.href}
+              key={link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black transition-colors duration-500 ease-in-out hover:text-white">
+              {link.icon}
+            </a>
+          ))}
+        </div>
+        <a href="#privacy-policy" className="text-center text-sm hover:underline md:text-right">Privacy Policy</a>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
